5ff58d9fefa1c17a0a77320021ef0a4719533254
Jonas Frank <jonas@jkfrank.dk>
2019-10-20 12:22:08 +0200

    security commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav>
+        <v-toolbar dark color="deep-purple">
+           <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title>
+                <span class="font-weight-light">Project</span>
+                <span>Management</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn depressed color="deep-purple">
+               <span>Log ud</span>
+                <v-icon right>exit_to_app</v-icon>
+            </v-btn>
+            
+        </v-toolbar>
+
+        <v-navigation-drawer v-model="drawer" class="primary" >
+            <p>Punkt nummer 1</p>
+        </v-navigation-drawer>
+    </nav>
+</template>
+
+<script>
+export default {
+    data (){
+        return {
+            drawer: false
+        }
+    }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   vuetify,
+  icons: {
+    iconfon: 'md',
+  },
   render: h => h(App)
 }).$mount('#app')
