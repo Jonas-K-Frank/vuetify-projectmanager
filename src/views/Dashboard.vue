998757ce36ebf33db33260a8296a9d03a55ce952
Jonas Frank <jonasdgfrank@gmail.com>
2019-10-25 15:44:27 +0200

    startet på kort til team medlemmerne

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,10 +1,28 @@
 <template>
   <div class="dashboard">
-<h1 class="subtitle-1 grey--text text--darken-3"> This is the Dashboard</h1>
+<h1 class="subheading grey--text"> Dashboard</h1>
 
 <v-container class="my-5">
+<v-layout row class="mb-3 mr-3">
 
- <v-card flat class="pa-3" v-for="project in projects" :key="project.title">
+     <v-tooltip top>
+      <template v-slot:activator="{ on }">
+        <v-icon small>folder</v-icon>
+         <v-btn text small color="grey" v-on="on" @click="sortBy('title')" slot="activator" class="caption text-lowercase">sort by project</v-btn>
+      </template>
+      <span>Sort by project</span>
+    </v-tooltip>
+
+     <v-tooltip top>
+      <template v-slot:activator="{ on }">
+        <v-btn text small color="grey" v-on="on" @click="sortBy('person')" slot="activator" class="caption text-lowercase">sort by Person</v-btn>
+        <v-icon small left>person</v-icon>
+      </template>
+      <span>Sort by person name</span>
+    </v-tooltip>
+
+</v-layout>
+ <v-card flat class="pl-3" v-for="project in projects" :key="project.title">
    <v-layout row wrap :class="`pa-3 project ${project.status}`">
      <v-flex xs12 md6>
         <div class="caption grey--text">Project title</div>
@@ -19,11 +37,13 @@
         <div>{{ project.due }}</div> 
       </v-flex>
            <v-flex xs2 sm4 md2>
-        <div class="caption grey--text">status</div>
-        <div>{{ project.status}}</div>
+        <div>
+           <v-chip small :color="`${project.status}`" :class="`v-chip--active white--text caption my-2`">{{ project.status}}</v-chip>
+            
+           </div>
       </v-flex>
    </v-layout>
-   <v-divider :inset="inset"></v-divider>
+   <v-divider></v-divider>
   </v-card>
 </v-container>
   </div>
@@ -35,13 +55,18 @@
 
 export default {
   data() {
-    return{
+      return {
       projects: [
         { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
         { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
         { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
         { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
       ]
+    }
+  },
+  methods: {
+    sortBy(prop) {
+      this.projects.sort((a,b) => a[prop] < b[prop] ? -1 : 1)
     }
   }
 }
@@ -55,5 +80,14 @@ export default {
 }
 .project.overdue {
   border-left: 4px solid tomato;
+}
+.v-chip.complete {
+  background:  #3cd1c2;
+}
+.v-chip.ongoing {
+  background: #ffaa2c;
+}
+.v-chip.overdue {
+  background: #f83e70;
 }
 </style>
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,8 +1,26 @@
 <template>
   <div class="team">
-<h1 class="subtitle-1 grey--text text--darken-3"> This is the team page</h1>
+<h1 class="subtitle-1 grey--text text--darken-3"> Team</h1>
 <v-container class="my-5">
-  content
+  <v-layout row wrap>
+    <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+       <v-card  flat class="text-center ma-3">
+         <v-responsive class="pt-4">
+           images goes here
+         </v-responsive>
+         <v-card-text>
+           <div class="subheading">{{ person.name }}</div>
+           <div class="grey--text">{{ person.role }}</div>
+         </v-card-text>
+         <v-card-actions>
+           <v-btn text small color="grey">
+             <v-icon>message</v-icon>
+             <span>send message</span>
+           </v-btn>
+         </v-card-actions>
+       </v-card>
+    </v-flex>
+  </v-layout>
 </v-container>
   </div>
 </template>
@@ -12,9 +30,16 @@
 
 
 export default {
-  name: 'team',
-  components: {
-    
+ data() {
+    return {
+      team: [
+        { name: 'The Net Ninja', role: 'Web developer' },
+        { name: 'Ryu', role: 'Graphic designer' },
+        { name: 'Chun Li', role: 'Web developer' },
+        { name: 'Gouken', role: 'Social media maverick' },
+        { name: 'Yoshi', role: 'Sales guru'}
+      ]
+    }
   }
 }
 </script>
